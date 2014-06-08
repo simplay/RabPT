2aeea7bc00e1ec5bca393eb7a4db9dc8400f73ff
simplay <michael.single@students.unibe.ch>
2014-06-08 17:04:23 +0200

    Added description for stable inverse computation

--- util/matrix4f.rb
+++ util/matrix4f.rb
@@ -122,84 +122,12 @@ require "pry"
     build_schema  
   end
   
-  def lu_decomp
-    lu = s_copy
-    piv = []
-    
-    (1..4).each do |idx| 
-      pix << idx
-    end
-    pivsign = 1;
-    
-    #double[] 
-    lu_row_i = [];
-    
-    # double[] new double[m];
-    lu_col_j = []
-    
-    (1..4).each do |j|
-      # Make a copy of the j-th column to localize references.
-      # i-th element of j-th column of LU is ith index in array LUcolj
-      lu_col_j = []
-      (1..4).each do |i|
-        lu_col_j < lu.at(i,j);
-      end
-      
-      # Apply previous transformations.
-      (1..4).each do |i|
-        
-        # LUrowi = LU[i];
-        row_i_lu = lu.row(i)
-        lu_row_i = []
-        lu_row_i << row_i_lu.x
-        lu_row_i << row_i_lu.y
-        lu_row_i << row_i_lu.z
-        lu_row_i << row_i_lu.w
-
-        # Most of the time is spent in the following dot product.
-        kmax = [i, j].min
-        s = 0.0;
-        (1..kmax).each do |k|
-          s += lu_row_i[k-1] * lu_col_j[k-1];
-        end
-        lu_row_i[j-1] = lu_col_j[i-1] -= s;
-      end
-
-      # Find pivot and exchange if necessary.
-      p = j
-      ((j+1)..4).each do |i|
-        p = i if(lu_col_j[i-1].abs > lu_col_j[p-1].abs)
-      end
-      
-      if (p != j)
-        (1..n).each do |k|
-          t = lu.at(p,k)
-          lu.setElementAt(p,k, lu.at(j,k))
-          lu.setElementAt(j,k, t)          
-        end
-        k = piv[p]
-        piv[p-1] = piv[j-1]
-        piv[j-1] = k
-        pivsign = -pivsign
-      end
-      
-      # Compute multipliers.   
-      if(j < m && lu.at(j,j) != 0.0)
-        ((j+1)..4).each do |i|
-          lu_ii = lu.at(j,j)
-          lu_ij = lu.at(i,j)
-          normalized_val = lu_ij.to_f / lu_ii.to_f
-          lu.setElementAt(j,k, normalized_val)           
-        end
-      end
-    end
-    lu
-  end
-  
+ 
+  # Lower triangular matrix L resulting by 
+  # lu decomposition: A = L*R
   def L_matrix
     l = Matrix4f.new
     lu = lu_decomp
-    
     (1..4).each do |i|
       (1..4).each do |j|
         if (i > j)
@@ -214,10 +142,11 @@ require "pry"
     l
   end
   
+  # Upper triangular matrix U resulting by 
+  # lu decomposition: A = L*R
   def U_matrix
     u = Matrix4f.new
     lu = lu_decomp
-    
     (1..4).each do |i|
       (1..4).each do |j|
         if (i <= j)
@@ -230,7 +159,26 @@ require "pry"
     r 
   end
   
-  
+  # Given an equation system Ax = y
+  # assumption A is N x N matrix, N=4
+  # where x the unknown
+  # and x, y are both N x 1 vectors.
+  # Apply LU-Decomposition on A, then we 
+  # get [L,U] = lu(A), i.e.
+  # A = LU 
+  # Thus, we can reformulate our system 
+  # as: (LU)x = y
+  # which gives us a system of equation systems [SeS]:
+  # 1. || Lc = y ||
+  # 2. || Ux = x ||
+  # which allows us to solve for the vector x
+  # Our goal: Compute A^-1 of our given A
+  # by using this trick from above:
+  # Let I denote the 4x4 identiy matrix
+  # and [e1, e2, e3, e4] its column vectors
+  # foreach e_k do
+  #   set x = e_k and solve then [SeS]
+  #   x is then k-th column of A-^1
   def invert2
   end
   
@@ -364,6 +312,80 @@ require "pry"
     [@m20, @m21, @m22, @m23],
     [@m30, @m31, @m32, @m33]]
     self
+  end
+  
+  def lu_decomp
+    lu = s_copy
+    piv = []
+    
+    (1..4).each do |idx| 
+      pix << idx
+    end
+    pivsign = 1;
+    
+    #double[] 
+    lu_row_i = [];
+    
+    # double[] new double[m];
+    lu_col_j = []
+    
+    (1..4).each do |j|
+      # Make a copy of the j-th column to localize references.
+      # i-th element of j-th column of LU is ith index in array LUcolj
+      lu_col_j = []
+      (1..4).each do |i|
+        lu_col_j < lu.at(i,j);
+      end
+      
+      # Apply previous transformations.
+      (1..4).each do |i|
+        
+        # LUrowi = LU[i];
+        row_i_lu = lu.row(i)
+        lu_row_i = []
+        lu_row_i << row_i_lu.x
+        lu_row_i << row_i_lu.y
+        lu_row_i << row_i_lu.z
+        lu_row_i << row_i_lu.w
+
+        # Most of the time is spent in the following dot product.
+        kmax = [i, j].min
+        s = 0.0;
+        (1..kmax).each do |k|
+          s += lu_row_i[k-1] * lu_col_j[k-1];
+        end
+        lu_row_i[j-1] = lu_col_j[i-1] -= s;
+      end
+
+      # Find pivot and exchange if necessary.
+      p = j
+      ((j+1)..4).each do |i|
+        p = i if(lu_col_j[i-1].abs > lu_col_j[p-1].abs)
+      end
+      
+      if (p != j)
+        (1..n).each do |k|
+          t = lu.at(p,k)
+          lu.setElementAt(p,k, lu.at(j,k))
+          lu.setElementAt(j,k, t)          
+        end
+        k = piv[p]
+        piv[p-1] = piv[j-1]
+        piv[j-1] = k
+        pivsign = -pivsign
+      end
+      
+      # Compute multipliers.   
+      if(j < m && lu.at(j,j) != 0.0)
+        ((j+1)..4).each do |i|
+          lu_ii = lu.at(j,j)
+          lu_ij = lu.at(i,j)
+          normalized_val = lu_ij.to_f / lu_ii.to_f
+          lu.setElementAt(j,k, normalized_val)           
+        end
+      end
+    end
+    lu
   end
   
   alias_method :at, :elementAt
