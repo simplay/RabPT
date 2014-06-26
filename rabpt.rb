b9dbad0852547c92270ec37520b9fb848bb2ecba
panmari <smmuzi@gmail.com>
2014-06-26 15:32:47 +0200

    improved on argument parsing

--- rabpt.rb
+++ rabpt.rb
@@ -10,6 +10,9 @@ user_args = {}
 opt_parser = OptionParser.new do |opt|
   opt.banner = "TODO usage hints"
   opt.separator  ""
+  # some defaults
+  user_args[:SPP] = 8
+
   opt.on("-s", "--spp N", Integer, "the number of samples per pixel") do |spp|
     user_args[:SPP] = spp
   end
@@ -19,14 +22,31 @@ opt_parser = OptionParser.new do |opt|
   opt.on("-h", "--height HEIGHT", Integer, "height of the resulting image") do |height|
     user_args[:N] = height
   end
-  opt.on("-f", "--filename [FILENAME]", String, "name of the file that the resulting image will be saved in (without file extension)") do |file_name|
+  opt.on("-f", "--filename FILENAME", String, "name of the file that the resulting image will be saved in (without file extension)") do |file_name|
     user_args[:file_name] = file_name
   end
   opt.on("-i", "--inputscene N", Integer, "Number of the scene that is going to be rendered") do |selected_scene|
     user_args[:selected_scene] = selected_scene
   end
+
+  opt.on_tail("-h", "--help", "Show this message") do
+    puts opt
+    exit
+  end
+  opt.on_tail("--version", "Show version") do
+    puts ::Version.join('.')
+    exit
+  end
+end
+begin 
+  opt_parser.parse!
+  required_args = [:SPP, :M, :N, :file_name, :selected_scene]
+  required_args.each do |arg|
+    raise OptionParser::MissingArgument if user_args[arg].nil?
+  end
+rescue OptionParser::MissingArgument
+  puts opt_parser.help
+  exit
 end
 
-opt_parser.parse!
-puts user_args
 Renderer.new user_args
