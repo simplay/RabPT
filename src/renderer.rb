14203fecd04cef115b38053a42706c9138dc75dc
simplay <silent.simplay@gmail.com>
2018-03-03 22:20:39 +0100

    Cleanup start script and Renderer constructor

--- src/renderer.rb
+++ src/renderer.rb
@@ -20,51 +20,64 @@ class Renderer
     CORE_POOL_THREADS = Runtime.getRuntime.availableProcessors
   end
 
-  attr_accessor :image, :dimN, :dimM, :scene,
-                :integrator, :sampler
+  attr_accessor :image,
+                :height,
+                :width,
+                :scene,
+                :integrator,
+                :sampler
 
   def initialize(args={})
-    @dimN       = args[:N].to_i
-    @dimM       = args[:M].to_i
-    @scene      = scene_instance_from(args[:selected_scene] || 1, args[:SPP].to_i || 1)
-    @integrator = @scene.integrator_factory.make(@scene)
-    @sampler    = @scene.sampler_factory.make
-    @image      = Image.new(@dimN, @dimM)
+    @height     = args[:width]
+    @width      = args[:height]
+    @scene      = build_scene(args)
+    @integrator = scene.integrator_factory.make(scene)
+    @sampler    = scene.sampler_factory.make
+    @image      = Image.new(height, width)
 
-    puts "Start rendering \'#{@scene.base_name}\' at #{@dimN} by #{@dimM} pixels."
-    tic         = Time.now
+    puts "Start rendering \'#{scene.base_name}\' at #{height} by #{width} pixels."
+    tic = Time.now
     init_rendering_process
-    toc         = Time.now
+    toc = Time.now
 
     begin
-      user_input = (args[:file_name].nil? || args[:file_name].empty?) ? '' : "_#{args[:file_name].to_s}"
-      file_name = @scene.file_name + user_input
+      user_input = "_#{args[:file_name]}"
+      file_name = scene.file_name
+      file_name += user_input unless args[:file_name].empty?
+
       unless ENV['SKIP_WRITE']
-        @image.save("#{OUTPUT_PATH}#{file_name}.bmp", :bmp)
+        image.save("#{OUTPUT_PATH}#{file_name}.bmp", :bmp)
       end
     rescue
       print 'Could no generate the image'
     end
-    puts "\nIt took #{(toc-tic)*1000.0} ms to render this scene."
+
+    puts
+    elapsed_time = (toc - tic)
+    printf("Took %.2fs to render this scene.", elapsed_time)
+    puts
   end
 
   private
 
-  def scene_instance_from(selection, spp)
-    case selection
-    when 1
-      CameraTestScene.new(@dimM, @dimN, spp)
-    when 2
-      BlinnTestScene.new(@dimM, @dimN, spp)
-    when 3
-      InstancingTestScene.new(@dimM, @dimN, spp)
-    when 4
-      MeshLoadingTestScene.new(@dimM, @dimN, spp)
-    when 5
-      RefractionTestScene.new(@dimN, @dimN, spp)
-    else
-      CameraTestScene.new(@dimM, @dimN, spp)
-    end
+  def build_scene(args)
+    scene_klass = case args[:selected_scene].to_i
+                  when 1
+                    CameraTestScene
+                  when 2
+                    BlinnTestScene
+                  when 3
+                    InstancingTestScene
+                  when 4
+                    MeshLoadingTestScene
+                  when 5
+                    RefractionTestScene
+                  else
+                    CameraTestScene
+                  end
+
+    spp = args[:spp] || 1
+    scene_klass.new(width, height, spp)
   end
 
   def init_rendering_process
@@ -91,22 +104,22 @@ class Renderer
     block = {}
     tasks = []
     block[:xmin] = 1
-    block[:xmax] = @scene.width
+    block[:xmax] = scene.width
     delta_height = ROW_NUMBER_PER_THREAD
-    num_tasks = (@scene.height / delta_height).to_i
-    reminder_rows = @scene.height - num_tasks * delta_height
+    num_tasks = (scene.height / delta_height).to_i
+    reminder_rows = scene.height - num_tasks * delta_height
 
     num_tasks.times do |k|
       block[:ymin] = k * delta_height + 1
       block[:ymax] = (k + 1) * delta_height
-      tasks << FutureTask.new(RenderingTask.new(block, @scene, @integrator, @sampler))
+      tasks << FutureTask.new(RenderingTask.new(block, scene, integrator, sampler))
     end
 
     # handle reminder rows
     if reminder_rows > 0
-      block[:ymin] = @scene.height - reminder_rows + 1
-      block[:ymax] = @scene.height + 1
-      tasks << FutureTask.new(RenderingTask.new(block, @scene, @integrator, @sampler))
+      block[:ymin] = scene.height - reminder_rows + 1
+      block[:ymax] = scene.height + 1
+      tasks << FutureTask.new(RenderingTask.new(block, scene, integrator, sampler))
     end
 
     print 'Progress: '
@@ -126,7 +139,7 @@ class Renderer
   end
 
   def write_image
-    film_img = @scene.film.image
+    film_img = scene.film.image
     idn = 0
     idm = 0
 
@@ -134,10 +147,10 @@ class Renderer
     film_img.each do |row|
       idm = 0
       row.each do |pixel|
-        x = Renderer.toInt256(pixel.r)
-        y = Renderer.toInt256(pixel.g)
-        z = Renderer.toInt256(pixel.b)
-        @image[idm, idn] =  Color.from_rgb(x, y, z)
+        x = toInt255(pixel.r)
+        y = toInt255(pixel.g)
+        z = toInt255(pixel.b)
+        image[idm, idn] =  Color.from_rgb(x, y, z)
         idm += 1
       end
       idn += 1
@@ -148,22 +161,22 @@ class Renderer
   def compute_contribution
     counter = 0
     print "Progress: "
-    (1..@scene.width).each do |j|
-      (1..@scene.height).each do |i|
-        samples = @integrator.make_pixel_samples(@sampler, @scene.spp);
+    (1..scene.width).each do |j|
+      (1..scene.height).each do |i|
+        samples = integrator.make_pixel_samples(sampler, scene.spp);
         # for N sampels per pixel
-        (1..samples.length).each do |k|
+        samples.length.times do |k|
 
           # make ray
-          ray = @scene.camera.make_world_space_ray(i, j, samples[k-1])
+          ray = scene.camera.make_world_space_ray(i, j, samples[k])
 
           # evaluate ray
-          ray_spectrum = @integrator.integrate(ray)
+          ray_spectrum = integrator.integrate(ray)
 
           # write to film
-          @scene.film.add_sample(
-            i.to_f + samples[k-1][0].to_f,
-            j.to_f + samples[k-1][1].to_f,
+          scene.film.add_sample(
+            i.to_f + samples[k][0].to_f,
+            j.to_f + samples[k][1].to_f,
             ray_spectrum
           )
         end
@@ -176,8 +189,8 @@ class Renderer
   def interval2
     stars = PROGRESS_STAR_COUNT
 
-    num_tasks = (@scene.height / ROW_NUMBER_PER_THREAD).to_i
-    reminder_rows = @scene.height - num_tasks*ROW_NUMBER_PER_THREAD
+    num_tasks = (scene.height / ROW_NUMBER_PER_THREAD).to_i
+    reminder_rows = scene.height - num_tasks*ROW_NUMBER_PER_THREAD
     num_tasks += 1 if reminder_rows > 0
     pixels_per_star = (num_tasks / stars).to_i
     (pixels_per_star < 1) ? 1 : pixels_per_star
@@ -185,7 +198,7 @@ class Renderer
 
   def interval
     stars = PROGRESS_STAR_COUNT
-    pixel_count = @scene.width * @scene.height
+    pixel_count = scene.width * scene.height
     pixels_per_star = (pixel_count / stars).to_i
     (pixels_per_star < 1) ? 1 : pixels_per_star
   end
@@ -199,10 +212,12 @@ class Renderer
     (pixels % interval == 0) ? "* " : ""
   end
 
-  # TODO write a toneMAPPER instead of using this mapping from float unit range
-  # to int 0-255 range
-  # f: [0.0,1.0] -> [0,255]
-  def self.toInt256 f_value
-    (f_value*255).to_i
+  # TODO write a toneMAPPER instead of using this mapping from float unit range.
+  #
+  # Transforms a given value in the range [0, 1] to the range [0, 255].
+  # @param f_value [Float] value in [0, 1]
+  # @return [Integer] transformed value in range [0, 255]
+  def toInt255(f_value)
+    (f_value * 255).to_i
   end
 end
